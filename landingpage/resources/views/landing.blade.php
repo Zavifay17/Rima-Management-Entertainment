2e03506a1b8369f524f98a73beccd16783a8cf1c
RME Admin <admin@rme.com>
2026-09-02 21:16:27 +0700

    Fix floating WA mobile view with cache buster and media query

--- landingpage/resources/views/landing.blade.php
+++ landingpage/resources/views/landing.blade.php
@@ -30,7 +30,7 @@
     
     <!-- Stylesheet -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
     
     <!-- Flatpickr CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
