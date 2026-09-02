2bda41bb18dc47e21317106db05359ae46c11c08
RME Admin <admin@rme.com>
2026-09-02 23:50:55 +0700

    Update sponsor logos to use transparent PNGs

--- landingpage/resources/views/landing.blade.php
+++ landingpage/resources/views/landing.blade.php
@@ -1076,8 +1076,8 @@
                 </a>
                 <p class="footer-desc">Partner terpercaya penyedia kelengkapan panggung, tata suara, tata cahaya, dan visual berstandar tinggi di Indonesia.</p>
                 <div class="social-links sponsor-logos">
-                    <a href="#" aria-label="Sponsor 1"><img src="{{ asset('images/sponsors/sponsor1.jpg') }}" alt="Sponsor 1" title="Dinas Kebudayaan"></a>
-                    <a href="#" aria-label="Sponsor 2"><img src="{{ asset('images/sponsors/sponsor2.jpg') }}" alt="Sponsor 2" title="Festival Budaya"></a>
+                    <a href="#" aria-label="Sponsor 1"><img src="{{ asset('images/sponsors/sponsor1.png') }}" alt="Sponsor 1" title="Dinas Kebudayaan"></a>
+                    <a href="#" aria-label="Sponsor 2"><img src="{{ asset('images/sponsors/sponsor2.png') }}" alt="Sponsor 2" title="Festival Budaya"></a>
                     <a href="#" aria-label="Sponsor 3"><img src="{{ asset('images/sponsors/sponsor3.png') }}" alt="Sponsor 3" title="IPSI"></a>
                     <a href="#" aria-label="Sponsor 4"><img src="{{ asset('images/sponsors/sponsor4.png') }}" alt="Sponsor 4" title="Enjoy Jakarta"></a>
                 </div>
